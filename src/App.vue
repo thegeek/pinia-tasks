45c6463eabe9b627a04a4fc867b16e5fcd67263c
Francesco Fienga <f.fienga@gmail.com>
2023-07-08 21:50:38 +0200

    Header and Style.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <main></main>
+  <main>
+    <!-- Heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="Pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
 </template>
 
 <script setup></script>

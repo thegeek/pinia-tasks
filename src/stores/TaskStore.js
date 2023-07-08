17663826b4e0b1ae876273211da0f14bc387e33c
Francesco Fienga <f.fienga@gmail.com>
2023-07-08 22:04:52 +0200

    Add Pinia and create TaskStore.

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia';
+
+export const useTaskStore = defineStore('taskStore', {
+  state: () => ({
+    tasks: [
+      { id: 1, title: 'buy some milk', isFav: false },
+      { id: 2, title: 'play some music', isFav: true },
+      { id: 3, title: 'walk around', isFav: false }
+    ],
+    name: 'Yoshi'
+  })
+});

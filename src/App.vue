5530fd122b1ec9ade6e66ec39c6d777b3a0758dd
Francesco Fienga <f.fienga@gmail.com>
2023-07-08 22:20:43 +0200

    Accessing Store State.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,18 @@
       <img src="./assets/pinia-logo.svg" alt="Pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- Task List -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { useTaskStore } from './stores/TaskStore';
+import TaskDetails from './components/TaskDetails.vue';
+const taskStore = useTaskStore();
+</script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,7 +6,6 @@ export const useTaskStore = defineStore('taskStore', {
       { id: 1, title: 'buy some milk', isFav: false },
       { id: 2, title: 'play some music', isFav: true },
       { id: 3, title: 'walk around', isFav: false }
-    ],
-    name: 'Yoshi'
+    ]
   })
 });

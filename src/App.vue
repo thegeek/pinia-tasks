7c8790c76fc78e56788d873c203b409e63d68eef
Francesco Fienga <f.fienga@gmail.com>
2023-07-09 19:02:47 +0200

    ResetState and StoreToRefs.

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,15 @@
       <button :class="{ selected: filter === 'favs' }" @click="filter = 'favs'">Fav Tasks</button>
     </nav>
     <!-- Loading -->
-    <div class="loading" v-if="taskStore.isLoading">Loading Tasks...</div>
+    <div class="loading" v-if="isLoading">Loading Tasks...</div>
     <!-- Task List -->
     <div class="task-list" v-else>
       <p>You have {{ taskList.count }} {{ taskList.type }} left to do</p>
-      <div v-for="task in taskList.tasks" :key="task.id">
+      <div v-for="task in taskList.query" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
+    <!-- <button @click="taskStore.$reset">Reset</button> -->
   </main>
 </template>
 
@@ -31,19 +32,22 @@ import { computed, ref } from 'vue';
 import { useTaskStore } from './stores/TaskStore';
 import TaskDetails from './components/TaskDetails.vue';
 import TaskForm from './components/TaskForm.vue';
+import { storeToRefs } from 'pinia';
 
 const taskStore = useTaskStore();
 
 taskStore.getTasks();
 
+const { tasks, isLoading, favs, totalCount, favCount } = storeToRefs(taskStore);
+
 const filter = ref('all');
 const taskList = computed(() => {
   switch (filter.value) {
     case 'favs':
-      return { tasks: taskStore.favs, type: 'Fav Tasks', count: taskStore.favCount };
+      return { query: favs.value, type: 'Fav Tasks', count: favCount };
 
     default:
-      return { tasks: taskStore.tasks, type: 'Tasks', count: taskStore.totalCount };
+      return { query: tasks.value, type: 'Tasks', count: totalCount };
   }
 });
 </script>
